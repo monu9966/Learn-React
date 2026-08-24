1a0a38262942df7c5be9dfa66e4cf549a31fe7f2
monu9966 <monukumar896914@gmail.com>
2026-08-24 18:12:46 +0530

    add some new functionality in smart calculator

--- src/SmartCalculator.jsx
+++ src/SmartCalculator.jsx
@@ -5,6 +5,7 @@ function SmartCalculator() {
     const [firstNumber, setFirstNuber] = useState(null);
     const [operator, setOperator] = useState(null);
     const [expression, setExpression] = useState("");
+    const [angleMode, setAngleMode] = useState("DEG");
 
     const handleNumber = (number) => {
         if (display === "0") {
@@ -52,6 +53,84 @@ function SmartCalculator() {
         setDisplay(String(result));
     };
 
+    const toRadians = (degree) => {
+        return degree * (Math.PI / 180);    
+    };
+
+    const sine = () => {
+        const number = Number(display);
+
+        const angle = angleMode === "DEG" ? toRadians(number) : number;
+
+        const result = Math.sin(angle);
+
+        setExpression(`sin(${number}${angleMode === "DEG" ? "°" : ""}) =`);
+        setDisplay(String(Number(result.toFixed(10))));
+    };
+
+    const cosine = () => {
+        const number = Number(display);
+
+        const angle =
+            angleMode === "DEG"
+            ? toRadians(number)
+            : number;
+
+        const result = Math.cos(angle);
+
+        setExpression(`cos(${number}${angleMode === "DEG" ? "°" : ""})`);
+        setDisplay(String(Number(result.toFixed(10))));
+        };
+
+    const tangent = () => {
+        const number = Number(display);
+
+        const angle =
+            angleMode === "DEG"
+            ? toRadians(number)
+            : number;
+
+        const result = Math.tan(angle);
+
+        setExpression(`tan(${number}${angleMode === "DEG" ? "°" : ""})`);
+        setDisplay(String(Number(result.toFixed(10))));
+        };
+
+    const logarithm = () => {
+        const number = Number(display);
+
+        if (number <= 0) {
+            setDisplay("Error");
+            return;
+        }
+
+        const result = Math.log10(number);
+
+        setExpression(`log(${number}) =`);
+        setDisplay(String(Number(result.toFixed(10))));
+    };
+
+    const naturalLog = () => {
+        const number = Number(display);
+
+        if (number <= 0) {
+            setDiplay("Error");
+            return;
+        }
+
+        const result = Math.log(number);
+
+        setExpression(`ln(${number}) =`);
+        setDisplay(String(Number(result.toFixed(10))));
+    };
+
+    const power = () => {
+        setFirstNuber(Number(display));
+        setOperator("^");
+        setExpression(`${display} ^`);
+        setDisplay("0");
+    };
+
     const deleteLast = () => {
         if (display.length === 1) {
             setDisplay("0");
@@ -90,6 +169,8 @@ function SmartCalculator() {
         }
 
         result = firstNumber / secondNumber
+    } else if (operator === "^") {
+        result = Math.pow(firstNumber, secondNumber);
     }
 
     setExpression(`${firstNumber} ${operator} ${secondNumber} =`);
@@ -112,7 +193,7 @@ function SmartCalculator() {
             <div className="w-full max-w-md bg-gray-900  rounded-3xl shadow-2xl p-5">
                 <h1 className="text-white text-2xl font-bold text-center mb-5">Smart Calculator</h1>
 
-                <div className="bg-gray-950 rounded-2xl p-5 mb-5 text-white min-h[110px]">
+                <div className="bg-gray-950 hover:bg-gray-800 rounded-2xl p-5 mb-5 text-white min-h[110px]">
                     <div className="text-right text-gray-400 text-lg min-h-7">{expression} {display}</div>
 
                     <div className="text-4xl text-right font-semibold overflow-x-auto">
@@ -121,6 +202,10 @@ function SmartCalculator() {
                 </div>
 
                 <div className="grid grid-cols-4 gap-3">
+                    <button onClick={() => setAngleMode(angleMode === "DEG" ? "RAD" : "DEG")} className="bg-blue-500 hover:bg-blue-600 text-white rounded-xl h-14 font-bold">
+                        {angleMode}
+                    </button>
+
                     <button onClick={clearCalculator} className="bg-red-500 hover:bg-red-600 text-white rounded-xl h-14 font-semibold cursor-pointer">
                         AC
                     </button>
@@ -129,52 +214,52 @@ function SmartCalculator() {
                         DEL
                     </button>
 
-                    <button onClick={percentage} className="bg-gray-700 hover:bg-gray-600 text-white rounded-xl h-14 cursor-pointer">
-                        %
-                    </button>
-
                     <button onClick={() => handleOperator("/")} className="bg-orange-500 hover:bg-orange-600 text-white rounded-xl h-14 cursor-pointer">
                         ÷
                     </button>
 
-                    <button className="bg-gray-700 hover:bg-gray-600 text-white rounded-xl h-14 cursor-pointer">
+                    <button onClick={percentage} className="bg-gray-700 hover:bg-gray-600 text-white rounded-xl h-14 cursor-pointer">
+                        %
+                    </button>
+
+                    <button onClick={sine} className="bg-gray-700 hover:bg-gray-600 text-white rounded-xl h-14 cursor-pointer">
                         sin
                     </button>
-                    <button className="bg-gray-700 hover:bg-gray-600 text-white rounded-xl h-14 cursor-pointer">
+                    <button onClick={cosine} className="bg-gray-700 hover:bg-gray-600 text-white rounded-xl h-14 cursor-pointer">
                         cos
-                    </button>
-                    <button className="bg-gray-700 hover:bg-gray-600 text-white rounded-xl h-14 cursor-pointer">
-                        tan
                     </button>
                     <button onClick={() => handleOperator("*")} className="bg-orange-500 hover:bg-orange-600 text-white rounded-xl h-14 font-semibold cursor-pointer">
                         x
                     </button>
+                    <button onClick={tangent} className="bg-gray-700 hover:bg-gray-600 text-white rounded-xl h-14 cursor-pointer">
+                        tan
+                    </button>
 
-                    <button className="bg-gray-700 hover:bg-gray-600 text-white rounded-xl h-14 font-semibold cursor-pointer">
+                    <button onClick={logarithm} className="bg-gray-700 hover:bg-gray-600 text-white rounded-xl h-14 font-semibold cursor-pointer">
                         log
                     </button>
-                    <button className="bg-gray-700 hover:bg-gray-600 text-white rounded-xl h-14 font-semibold cursor-pointer">
+                    <button onClick={naturalLog} className="bg-gray-700 hover:bg-gray-600 text-white rounded-xl h-14 font-semibold cursor-pointer">
                         ln
-                    </button>
-                    <button onClick={handleSqareRoot} className="bg-gray-700 hover:bg-gray-600 text-white rounded-xl h-14 font-semibold cursor-pointer">
-                        √
                     </button>
                     <button onClick={() => handleOperator("-")} className="bg-orange-500 hover:bg-orange-600 text-white rounded-xl h-14 font-semibold cursor-pointer">
                         -
+                    </button>
+                    <button onClick={handleSqareRoot} className="bg-gray-700 hover:bg-gray-600 text-white rounded-xl h-14 font-semibold cursor-pointer">
+                        √
                     </button>
 
                     <button onClick={handleSquare} className="bg-gray-700 hover:bg-gray-600 text-white rounded-xl h-14 cursor-pointer">
                         x²
                     </button>
-                    <button className="bg-gray-700 hover:bg-gray-600 text-white rounded-xl h-14 cursor-pointer">
+                    <button onClick={power} className="bg-gray-700 hover:bg-gray-600 text-white rounded-xl h-14 cursor-pointer">
                         xʸ
                     </button>
 
-                    <button onClick={insertPi} className="bg-gray-700 hover:bg-gray-600 text-white rounded-xl h-14 cursor-pointer">
-                        π
-                    </button>
                     <button onClick={() => handleOperator("+")} className="bg-orange-500 hover:bg-orange-600 text-white rounded-xl h-14 font-semibold cursor-pointer">
                         +
+                    </button>
+                    <button onClick={insertPi} className="bg-gray-700 hover:bg-gray-600 text-white rounded-xl h-14 cursor-pointer">
+                        π
                     </button>
 
                     <button onClick={() => handleNumber("7")} className="bg-gray-700 hover:bg-gray-600 text-white rounded-xl h-14 cursor-pointer">
@@ -186,7 +271,7 @@ function SmartCalculator() {
                     <button onClick={() => handleNumber("9")} className="bg-gray-700 hover:bg-gray-600 text-white rounded-xl h-14 cursor-pointer">
                         9
                     </button>
-                    <button onClick={() => handleNumber("+")} className="bg-gray-700 hover:bg-gray-600 text-white rounded-xl h-14 font-semibold cursor-pointer">
+                    <button className="bg-gray-700 hover:bg-gray-600 text-white rounded-xl h-14 font-semibold cursor-pointer">
                         (
                     </button>
                     <button onClick={() => handleNumber("4")} className="bg-gray-700 hover:bg-gray-600 text-white rounded-xl h-14 cursor-pointer">
@@ -213,7 +298,7 @@ function SmartCalculator() {
                     <button onClick={handleDecimal} className="bg-gray-700 hover:bg-gray-600 text-white rounded-xl h-14 cursor-pointer">
                         .
                     </button>
-                    <button onClick={() => handleNumber("0")} className="bg-gray-700 hover:bg-gray-600 text-white rounded-xl h-14 cursor-pointer col-span-2">
+                    <button onClick={() => handleNumber("0")} className="bg-gray-700 hover:bg-gray-600 text-white rounded-xl h-14 cursor-pointer col-span-1">
                         0
                     </button>
 
